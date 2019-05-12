f2a1071f6576d0dad461f616fe7fa64f1f9a3834
mateo2411 <47614981+mateo2411@users.noreply.github.com>
2019-05-12 17:57:58 -0500

    Create Librerias.h

---
+++ Librerias.h
@@ -0,0 +1,4 @@
+void MovParabolico(float VelocidadIni, float PosIniY, double Angulo, float Tiempo);
+void ChoqueElastico(float VelIniM1, float VelIniM2, float Masa1, float Masa2);
+void Rebotes(float h, float m);
+void MovParabolicoFricc(float VelocidadIni, float PosIniY, double Angulo, float Tiempo, float fricc, float R, float masa);
